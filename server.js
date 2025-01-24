1515240519cc823753f17091580cea7b0356edd9
ganeshArwat <ganesharwat123@gmail.com>
2025-01-24 15:02:48 +0530

    natours api started

---
+++ server.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB connection successful');
+  });
+
+// console.log(process.env);
+// npm i eslint prettier eslint-config-prettier eslint-plugin-prettier eslint-config-airbnb eslint-plugin-node eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react --save-dev
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`);
+});
